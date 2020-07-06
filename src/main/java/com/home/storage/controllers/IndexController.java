d00097965a8e0c252cb8476783984e2180d82704
Aga <aga.ltk@gmail.com>
2020-07-06 22:20:42 +0200

    index html and controller #6

--- src/main/java/com/home/storage/controllers/IndexController.java
+++ src/main/java/com/home/storage/controllers/IndexController.java
@@ -1,14 +1,15 @@
 package com.home.storage.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
-
+    @RequestMapping({"", "/", "index","index.html"})
+    public String index(Model model){
+        model.addAttribute("welcome", "Home Storage app");
         return "index";
     }
 }
